cf1df123ad227f4a117628b1c15ec9e6e6037207
Brenden Reeves <brenden.reeves@codescientists.com>
2021-10-20 22:07:53 -0500

    Added YoutubeTogether and Removed Dangerous Stuff

---
+++ discord/commands/youtubeTogether.js
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const axios = require('axios')
+const { token } = require('../config.json')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('youtube-together')
+        .setDescription('Starts a youtube together activity in the specified voice channel')
+        .addChannelOption((option) => option
+            .setName('target')
+            .setDescription('The voice channel to start an activity in')
+            .setRequired(true)),
+    async execute(interaction) {
+        const channel = interaction.options.getChannel('target')
+        if (channel && !channel.isText()) {
+            axios.post(`https://discord.com/api/v8/channels/${channel.id}/invites`,
+                {
+                    max_age: 86400,
+                    max_uses: 0,
+                    target_application_id: '755600276941176913', // youtube together
+                    target_type: 2,
+                    temporary: false,
+                    validate: null,
+                }, {
+                    headers: {
+                        Authorization: `Bot ${token}`,
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then((response) => {
+                    const invite = response.data
+                    if (invite.error || !invite.code) { return interaction.reply('Unable to start a **Youtube Together** activity in the specific channel at this time.') }
+                    return interaction.reply(`And done! **YouTube Together** has been started in [${channel.name}](https://discord.gg/${invite.code})`)
+                })
+                .catch(() => interaction.reply('Unable to start a **Youtube Together** activity in the specific channel at this time.'))
+        } else {
+            return interaction.reply('You did not specify a voice channel. Please try again.')
+        }
+    },
+}
---
+++ discord/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+const fs = require('fs')
+const { Client, Collection, Intents } = require('discord.js')
+const { token } = require('./config.json')
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
+
+commandFiles.forEach((file) => {
+    // eslint-disable-next-line global-require
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.data.name, command)
+})
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return
+
+    const command = client.commands.get(interaction.commandName)
+
+    if (!command) return
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+        return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+})
+
+client.login(token)
