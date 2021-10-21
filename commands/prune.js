0b97e621169877290d02fb514c6f6bd38ecde5bd
Brenden Reeves <brenden.reeves@codescientists.com>
2021-10-21 00:36:31 -0500

    Fixed Pruning Logic

--- commands/prune.js
+++ commands/prune.js
@@ -9,7 +9,7 @@ module.exports = {
             .setName('amount')
             .setDescription('Number of messages to prune')
             .setRequired(true))
-        .addIntegerOption((option) => option
+        .addChannelOption((option) => option
             .setName('channel')
             .setDescription('Channel to remove messages from')
             .setRequired(false)),
@@ -30,21 +30,23 @@ module.exports = {
         const channel = interaction.options.getChannel('channel')
 
         if (amount <= 1 || amount > 100) {
-            return interaction.reply({ content: 'You need to input a number between 1 and 99.', ephemeral: true })
+            return interaction.reply({ content: 'You need to input a number between 2 and 99.', ephemeral: true })
         }
         if (channel) {
             if (!channel.isText()) { return interaction.reply({ content: `Unable to prune messages. ${channel.name} is not a text channel` }) }
-            await channel.bulkDelete(amount).catch((error) => {
-                console.error(error)
-                interaction.reply({ content: `There was an error trying to prune messages in ${channel.name}`, ephemeral: true })
-            })
+            await channel.bulkDelete(amount)
+                .catch((error) => {
+                    console.error(error)
+                    interaction.reply({ content: `There was an error trying to prune messages in ${channel.name}`, ephemeral: true })
+                })
         } else {
             await interaction.channel.bulkDelete(amount, true)
-                .then(interaction.reply({ content: `Successfully pruned \`${amount}\` messages.`, ephemeral: true }))
                 .catch((error) => {
                     console.error(error)
                     interaction.reply({ content: 'There was an error trying to prune messages in this channel', ephemeral: true })
                 })
         }
+
+        interaction.reply({ content: `Successfully pruned \`${amount}\` messages.`, ephemeral: true })
     },
 }
