5d79a3049b21cbc3d822558d33235ba4054d2c5c
Brenden Reeves <brenden.reeves@codescientists.com>
2021-10-21 11:44:10 -0500

    Added Firebase and DigitalOcean Spaces

--- index.js
+++ index.js
@@ -9,62 +9,88 @@ const { Interactions, Messages } = require('./events')
 
 dotenv.config()
 
-// Discord Client Setup
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_BANS,
-    ],
+// DigitalOcean Spaces Setup
+const spacesEndpoint = new aws.Endpoint('sfo3.digitaloceanspaces.com')
+const s3 = new aws.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.SPACES_KEY,
+    secretAccessKey: process.env.SPACES_SECRET,
 })
 
-// Setup Commands
-client.commands = new Collection()
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
+s3.getObject({ Bucket: 'isle-of-val', Key: 'firebase-admin.json' }, async (err, object) => {
+    if (err) {
+        throw new Error('Unable to connect to bucket to obtain firebase admin sdk info')
+    }
+    // Firebase Setup
+    await firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(JSON.parse(object.Body)),
+        databaseURL: 'https://isle-of-val-default-rtdb.firebaseio.com',
+    })
 
-const commands = []
-commandFiles.forEach((file) => {
-    // eslint-disable-next-line global-require
-    const command = require(`./commands/${file}`)
-    commands.push(command)
-    client.commands.set(command.data.name, command)
-})
+    const db = await firebaseAdmin.firestore().collection('krilebot')
 
-client.once('ready', async () => {
-    // Set Status Message
-    client.user.setActivity('Ejika\'s theories', { type: 'LISTENING' })
+    // Discord Client Setup
+    const client = new Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_INVITES,
+            Intents.FLAGS.GUILD_BANS,
+        ],
+    })
 
-    // Add Guild Command Permission
-    const guild = client.guilds.cache.get(process.env.GUILD_ID)
-    if (guild) {
-        const guildCommands = await guild.commands.fetch()
-        guildCommands.forEach((cmd) => {
-            const { permissions, data } = commands.find((x) => x.data.name === cmd.name)
-            if (permissions) {
-                permissions.forEach((perm) => {
-                    console.log(`Adding Permissions for ${data.name} | Type: ${perm.type} | ID of Restriction: ${perm.id}`)
-                })
-                cmd.permissions.add({ permissions })
-            }
+    // Setup Commands
+    client.commands = new Collection()
+    const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
+
+    const commands = []
+    commandFiles.forEach((file) => {
+        // eslint-disable-next-line global-require
+        const command = require(`./commands/${file}`)
+        commands.push(command)
+        client.commands.set(command.data.name, command)
+    })
+
+    client.once('ready', async () => {
+        // Set Status Message
+        const config = db.doc('globals')
+
+        config.get('status').then((result) => {
+            const { status } = result.data()
+            client.user.setActivity(status.message, status.type)
+        })
+
+        // Add Guild Command Permission
+        const guild = client.guilds.cache.get(process.env.GUILD_ID)
+        if (guild) {
+            const guildCommands = await guild.commands.fetch()
+            guildCommands.forEach((cmd) => {
+                const { permissions, data } = commands.find((x) => x.data.name === cmd.name)
+                if (permissions) {
+                    permissions.forEach((perm) => {
+                        console.log(`Adding Permissions for ${data.name} | Type: ${perm.type} | ID of Restriction: ${perm.id}`)
+                    })
+                    cmd.permissions.add({ permissions })
+                }
+            })
+        }
+        console.log('Bot is ready and online!')
+    })
+
+    // Event Processing
+    client.on(Interactions.incoming.name, async (interaction) => {
+        await Interactions.incoming.execute(client, interaction)
+    })
+
+    // Logging
+    if (process.env.ENABLE_LOGGING) {
+        client.on(Messages.deletion.name, (message) => {
+            const logChannel = client.channels.cache.get(process.env.LOGGING_CHANNEL)
+            Messages.deletion.execute(message, logChannel)
         })
     }
-    console.log('Bot is ready and online!')
+
+    // Start Bot
+    client.login(process.env.TOKEN)
 })
-
-// Event Processing
-client.on(Interactions.incoming.name, async (interaction) => {
-    await Interactions.incoming.execute(client, interaction)
-})
-
-// Logging
-if (process.env.ENABLE_LOGGING) {
-    client.on(Messages.deletion.name, (message) => {
-        const logChannel = client.channels.cache.get(process.env.LOGGING_CHANNEL)
-        Messages.deletion.execute(message, logChannel)
-    })
-}
-
-// Start Bot
-client.login(process.env.TOKEN)
